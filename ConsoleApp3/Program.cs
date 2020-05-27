b0131e75e6f41daa617bbb64bd03f4cc8c844127
kirill.dunec <kirill.dunec@gmail.com>
2020-05-27 19:54:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Drink;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WordAndDrink
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<Student> students = new List<Student>();
+                var path = @"..\input.txt";
+                var writePath = @"..\output.txt";
+            int school = 2;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string str;
+                while ((str = sr.ReadLine()) != null)
+                {
+                    Student temp = new Student(str);
+                    students.Add(temp);
+
+                }
+            }
+                
+                
+
+                
+                
+              foreach (var student in students)
+              {
+                Console.WriteLine(student);
+              }
+                                 
+                  
+          
+              // students.Sort();//с помощью реализации структурой интерфейса IComparable
+            
+               students.Sort(new StudentComparer());//помощью класса Comparer, унаследованного от интерфейса IComparer<T>
+            
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                foreach (var student in students)
+                {
+                    if (student.SchoolNumber == school )
+                    {
+                        sw.WriteLine(student);
+                    }
+                   
+
+                }
+            }
+            
+         
+            Console.ReadKey();
+            }
+        }
+    }
+
---
+++ ConsoleApp3/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drink
+{
+    class StudentComparer : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            if (x.DateOfBirth > y.DateOfBirth)
+                return 1;
+            else if (x.DateOfBirth < y.DateOfBirth)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+ 
+
+    class Student :IComparable
+    {
+
+        public DateTime DateOfBirth { get; }
+        public string Name { get; }
+        public string Addres{get; }
+        public int SchoolNumber { get; }
+        
+        
+        public Student(string str)
+        {
+            str.Trim();
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Name = words[0];
+            DateOfBirth = Convert.ToDateTime(words[1]);
+            Addres = words[2];
+            SchoolNumber = Convert.ToInt32(words[3]);
+            
+
+
+
+        }
+     
+
+        public override string ToString()
+        {
+            return Name+" " + DateOfBirth.ToString("dd.MM.yyyy") + " " + Addres +" "+ SchoolNumber;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Student student = obj as Student;
+            if (student != null)
+                return DateOfBirth.CompareTo(student.DateOfBirth);
+            else
+                throw new Exception("Невозможно сравнить два объекта");
+        }
+    }
+}
