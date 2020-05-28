6de3cf5b450128d491a4faf26a85b0961762e624
kirill.dunec <kirill.dunec@gmail.com>
2020-05-28 15:10:36 +0300

    9 and 8

---
+++ 9/FileService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    public class FileService
+    {
+        
+        object obj = new object();
+        string path = @"..\file.txt"; 
+        public Task WriteFile()
+        {
+            return Task.Run(() =>
+            {
+                lock (obj)
+                { 
+                    var rnd = new Random();
+                    Console.WriteLine("Start recording");
+                    using (var writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+                    {
+                        for (var i = 1; i < 10000000; i++)
+                        {
+                            writer.Write(rnd.Next(Int32.MaxValue));
+                        }
+                    }
+                    Console.WriteLine("Write has finished");
+                }
+            });
+        }
+        
+        public Task ReadFile()
+        {
+            return Task.Run(() =>
+            {
+                lock (obj) 
+                {
+                    Console.WriteLine("Start");
+                    using (var reader = new BinaryReader(File.OpenRead(path)))
+                    {
+                        try
+                        {
+                            while (true)
+                            {
+                                reader.ReadInt32(); 
+                            }
+                        }
+                        catch (EndOfStreamException)
+                        {
+                            Console.WriteLine("Read has finished");
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
+   
+
---
+++ 9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileService = new FileService();
+            Console.WriteLine("Запуск потоков");
+            var writer = fileService.WriteFile();
+            var reader = fileService.ReadFile();
+            Console.WriteLine("Ожидание завершения потоков");
+            Task.WaitAll(writer, reader);
+            Console.WriteLine("OK");
+            Console.ReadKey();
+        }
+    }
+}
